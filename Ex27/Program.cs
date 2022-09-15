bc98a6dfc1fa00397d976e438a83ef04372588fb
SevindzhK <sevinge0709@gmail.com>
2022-09-15 17:43:42 +0300

    Added Ex27

---
+++ Ex27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Enter the number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    if (num > 0)
+    {
+        while (num > 0)
+        {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine(SumDigit(number));
